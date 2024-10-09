e8d8b53cb0e37628848345a64b8b84c8f2e86011
abap34 <abap0002@gmail.com>
2024-10-09 17:31:51 +0900

    chore: better design

--- script/morning_remind.py
+++ script/morning_remind.py
@@ -1,14 +1,17 @@
 import json
-from task import Task
 import sys
-import tabulate
 import warnings
 
+import tabulate
+from task import Task
+
+
 def load_tasks() -> list[Task]:
-    with open('reminds/tasks.json', 'r') as f:
+    with open("reminds/tasks.json", "r") as f:
         task = json.load(f)
 
     return [Task.from_dict(t) for t in task]
+
 
 def task_list_message(tasks: list[Task]) -> str:
     """
@@ -18,34 +21,31 @@ def task_list_message(tasks: list[Task]) -> str:
     """
     table = []
     for task in tasks:
-        table.append(
-            task.to_dict(
-                exclude={"id", "done"}).values()
-        )
+        task_dict = task.to_dict(exclude={"id"})
+
+        # issue_number の前に "#" をつける
+        task_dict["issue_number"] = f"#{task_dict['issue_number']}"
+
+        # done を　✅ に変換
+        task_dict["done"] = "✅" if task_dict["done"] else ""
+
+        table.append(task_dict.values())
 
     return tabulate.tabulate(
-        table,
-        headers=task.to_dict(exclude={"id", "done"}).keys(),
-        tablefmt="github"
+        table, headers=task.to_dict(exclude={"id"}).keys(), tablefmt="github"
     )
 
 
-
 def main():
-    github_event_type = sys.argv[1]
-    if github_event_type == "issue_comment":
-        with open('comment.txt', 'r') as f:
-            if not '/tasks' in f.read():
-                warnings.warn("/tasks が含まれていません。スキップします。")
-                return
     tasks = load_tasks()
 
     # 残りのタスクを全て抽出
     remaining_tasks = [task for task in tasks if not task.done]
     message = task_list_message(remaining_tasks)
-  
+
     with open("message.txt", "w") as f:
         f.write(message)
+
 
 if __name__ == "__main__":
     main()
